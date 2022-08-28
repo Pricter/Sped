38c0253ba28e03bc171479417bcd5225abdc45ea
Pricter <kodithiyalaveekshithv2@gmail.com>
2022-08-28 10:46:00 +0530

    + -D_DEBUG

--- engine/build.bat
+++ engine/build.bat
@@ -10,7 +10,7 @@ SET assembly=engine
 SET compilerFlags=-g -shared -Wvarargs -Wall -Werror
 SET includeFlags=-Isrc -I%VULKAN_SDK%/Include
 SET linkerFlags=-luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
-SET defines=-DSPEDEXPORT -D_CRT_SECURE_NO_WARNINGS
+SET defines=-D_DEBUG -DSPEDEXPORT -D_CRT_SECURE_NO_WARNINGS
 
 ECHO "Building %assembly%%..."
 clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.dll %defines% %includeFlags% %linkerFlags%
